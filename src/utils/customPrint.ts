c4a4ca55cfbee97ba6f4afb9ed62788dbf870b7c
checkiamsiam <issiam02415@gmail.com>
2023-06-10 16:54:32 +0600

    pino logger setup

--- src/utils/customPrint.ts
+++ src/utils/customPrint.ts
@@ -4,7 +4,25 @@ import PinoPretty from "pino-pretty";
 
 const print = pino({
   transport: {
-    target: "pino-pretty",
+    targets: [
+      { target: "pino-pretty", level: "info", options: { colorize: true } },
+      {
+        level: "error",
+        target: "pino/file",
+        options: {
+          destination: "logs/app.error.log",
+          flags: "a",
+        },
+      },
+      {
+        level: "info",
+        target: "pino/file",
+        options: {
+          destination: "logs/app.info.log",
+          flags: "a",
+        },
+      },
+    ],
     options: {
       colorize: true,
     },
